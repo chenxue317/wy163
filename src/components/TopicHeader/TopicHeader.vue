a51020199829021ab0f770d0bdbf499d7b37773d
peiqi <xueman@163.com>
2019-07-21 21:33:25 +0800

    login

--- src/components/TopicHeader/TopicHeader.vue
+++ src/components/TopicHeader/TopicHeader.vue
@@ -4,9 +4,11 @@
       <router-link to="/" class="left-home">
         <i class="iconfont icon-shouye"></i>
       </router-link>
-      <div class="tab">
+      <div class="tab" v-if="$route.path==='/topic'||$route.path==='/expert'">
         <a href="javascript:;" class="discover" :class="{active:$route.path==='/topic'}" @click="$router.push('/topic')">发现</a>
         <a href="javascript:;" class="choice" :class="{active:$route.path==='/expert'}" @click="$router.push('/expert')">甄选家</a>
+      </div>
+      <div class="title" v-if="$route.path==='/profile'">
       </div>
       <div class="searchAndCart">
         <router-link to="/search"><i class="iconfont icon-search"></i></router-link>
@@ -39,10 +41,15 @@
     padding-right 26px
     box-sizing border-box
     display flex
+    align-items center
+    justify-content space-between
     font-size 28px
     border-bottom 1px solid #ccc
     .icon-shouye
-      font-size 42px
+      font-size 50px
+      position absolute
+      left 24px
+      top 0
     .tab
       display flex
       justify-content center
@@ -53,12 +60,18 @@
           color #b4282d
           font-size 34px
           font-weight 700
+    .title
+      width 160px
+      height 44px
+      margin 0 auto
+      background-image url(//yanxuan-static.nosdn.127.net/hxm/yanxuan-wap/p/20161201/style/img/icon-normal/indexLogo-a90bdaae6b.png)
+      background-size 100% 100%
     .searchAndCart
       position absolute
       right 26px
       top 0
       .iconfont
-        font-size 40px
+        font-size 45px
       .icon-search
         margin-right 20px
     
--- src/pages/Profile/Profile.vue
+++ src/pages/Profile/Profile.vue
@@ -1,15 +1,80 @@
 <template>
-  <div>
-   profile
+  <div class="profileContainer">
+   <TopicHeader/>
+   <div class="content">
+     <h2 class="logo">
+       <img src="//yanxuan.nosdn.127.net/bd139d2c42205f749cd4ab78fa3d6c60.png" alt="">
+     </h2>
+     <div class="btns">
+       <a href="javascript:;" class="phone-login active">
+         <i class="iconfont icon-phone"></i>
+         <span>手机号码登录</span>
+       </a>
+        <a href="javascript:;" class="pwd-login">
+          <i class="iconfont icon-email"></i>
+          <span>邮箱账号登录</span>
+       </a>
+     </div>
+     <div class="phone-link">
+       <a href="javascript:;">
+         <span>手机号快速注册</span>
+         <i class="iconfont icon-arrow"></i>
+       </a>
+     </div>
+   </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import TopicHeader from '../../components/TopicHeader/TopicHeader.vue'
   export default {
+    components:{
+      TopicHeader
+    }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" scoped>
-
+  .profileContainer
+    background-color #eee
+    height 100%
+    .content
+      padding-top 100px
+      .logo
+        padding 160px 0 
+        text-align center
+        img 
+          width 268px
+          height 90px
+      .btns
+        padding 0 40px
+        a 
+          width 100%
+          height 90px
+          line-height 90px
+          margin-bottom 32px
+          color #b4282d
+          border 1px solid #b4282d
+          font-size 30px
+          display flex
+          align-items center
+          justify-content center
+          &.active
+            background-color #b4282d
+            color #fff   
+          .iconfont
+            font-size 34px
+            margin-right 10px
+      .phone-link 
+        text-align center
+        a
+          display flex
+          justify-content center
+          align-items center
+          span 
+            color #333
+            font-size 28px
+          .iconfont
+            font-size 30px        
  
 </style>
